1e1eeaddd50a10c12800f43f00bf758874d1f01b
FlipSs <flipss1599@gmail.com>
2019-07-29 20:24:47 +0300

    log info refactor

--- src/logs/LogInfo.ts
+++ src/logs/LogInfo.ts
@@ -2,63 +2,56 @@ import {TypeUtils} from "../utils";
 import {ILogInfo} from "./ILogInfo";
 import {LoggableError} from "./LoggableError";
 import {LogMessageFactory} from "./LogMessageFactory";
+import {asEnumerable, Dictionary, HashSet, IDictionary, IHashSet} from "../collections";
 
-const methodLogInfo = '___method___log___info___';
-const methodLogInfoDisable = '___method___log___info___disable___';
+const customLogInfoDictionary: IDictionary<Function, IDictionary<string, ILogInfo>> = new Dictionary<Function, IDictionary<string, ILogInfo>>();
+const disabledLogInfoDictionary: IDictionary<Function, IHashSet<string>> = new Dictionary<Function, IHashSet<string>>();
 
 export function ClassLogInfo(info: ILogInfo) {
     return (target: Function) => {
         const prototype = target.prototype;
-        const excludedProperties = new Set<string>(['constructor', methodLogInfo, methodLogInfoDisable]);
-        if (methodLogInfoDisable in prototype) {
-            const disabledProperties = prototype[methodLogInfoDisable];
-            if (TypeUtils.is(disabledProperties, Array)) {
-                for (const property of disabledProperties) {
-                    if (TypeUtils.isString(property)) {
-                        excludedProperties.add(property);
-                    }
+
+        const excludedMethods = disabledLogInfoDictionary.getValueOrDefault(target) || new HashSet<string>();
+        excludedMethods.add('constructor');
+
+        const customValues = customLogInfoDictionary.getValueOrDefault(target);
+
+        const methods = asEnumerable(Object.getOwnPropertyNames(prototype))
+            .where(p => !excludedMethods.has(p))
+            .select(p => {
+                return {
+                    descriptor: Object.getOwnPropertyDescriptor(prototype, p),
+                    name: p
                 }
-            }
-        }
+            })
+            .where(p => !TypeUtils.isNullOrUndefined(p.descriptor) && TypeUtils.is(p.descriptor.value, Function) && p.descriptor.writable)
+            .toArray();
 
-        const methodLogInfoMap: Map<string, ILogInfo> = methodLogInfo in prototype &&
-            TypeUtils.is(prototype[methodLogInfo], Map) &&
-            prototype[methodLogInfo];
+        for (const method of methods) {
+            const methodValue = method.descriptor.value;
+            const methodLogInfo: ILogInfo = customValues && customValues.getValueOrDefault(method.name) || info;
 
-        for (const propertyName in prototype) {
-            if (!excludedProperties.has(propertyName) && prototype.hasOwnProperty(propertyName)) {
-                const propertyDescriptor = Object.getOwnPropertyDescriptor(prototype, propertyName);
-                if (!propertyDescriptor ||
-                    !TypeUtils.is(propertyDescriptor.value, Function) ||
-                    propertyDescriptor.get && !propertyDescriptor.set) {
-                    continue;
-                }
+            prototype[method.name] = (...args: any[]) => {
+                try {
+                    const result = methodValue.apply(this, args);
+                    if (!TypeUtils.isNullOrUndefined(result)) {
+                        return Promise.resolve(result).then(undefined, (reason) => {
+                            if (TypeUtils.is(reason, LoggableError)) {
+                                return Promise.reject(reason);
+                            }
 
-                const method = propertyDescriptor.value;
-                const methodLogInfo: ILogInfo = methodLogInfoMap.get(propertyName) as ILogInfo || info;
-
-                prototype[propertyName] = (...args: any[]) => {
-                    try {
-                        const methodResult = method.apply(this, args);
-                        if (!TypeUtils.isNullOrUndefined(methodResult)) {
-                            return Promise.resolve(methodResult).then(undefined, (reason) => {
-                                if (TypeUtils.is(reason, LoggableError)) {
-                                    return Promise.reject(reason);
-                                }
-
-                                const logMessage = LogMessageFactory.create(reason, methodLogInfo.category, methodLogInfo.logLevel);
-                                return Promise.reject(new LoggableError(logMessage));
-                            });
-                        }
-                    } catch (e) {
-                        if (TypeUtils.is(e, LoggableError)) {
-                            throw e;
-                        }
-
-                        const logMessage = LogMessageFactory.create(e, methodLogInfo.category, methodLogInfo.logLevel);
-
-                        throw new LoggableError(logMessage);
+                            const logMessage = LogMessageFactory.create(reason, methodLogInfo.category, methodLogInfo.logLevel);
+                            return Promise.reject(new LoggableError(logMessage));
+                        });
                     }
+                } catch (e) {
+                    if (TypeUtils.is(e, LoggableError)) {
+                        throw e;
+                    }
+
+                    const logMessage = LogMessageFactory.create(e, methodLogInfo.category, methodLogInfo.logLevel);
+
+                    throw new LoggableError(logMessage);
                 }
             }
         }
@@ -66,22 +59,18 @@ export function ClassLogInfo(info: ILogInfo) {
 }
 
 export function DisableMethodLogInfo() {
-    return (target: any, propertyName: string) => {
-        if (!target.hasOwnProperty(methodLogInfoDisable)) {
-            target[methodLogInfoDisable] = [];
-        }
-
-        target[methodLogInfoDisable].push(propertyName);
+    return (target: Function, propertyName: string) => {
+        disabledLogInfoDictionary
+            .getOrAdd(target, (target) => new HashSet<string>())
+            .add(propertyName);
     }
 }
 
 export function MethodLogInfo(info: ILogInfo) {
-    return (target: any, propertyName: string) => {
-        if (!target.hasOwnProperty(methodLogInfo)) {
-            target[methodLogInfo] = new Map<string, ILogInfo>();
-        }
-
-        target[methodLogInfo].set(propertyName, info);
+    return (target: Function, propertyName: string) => {
+        customLogInfoDictionary
+            .getOrAdd(target, (t) => new Dictionary<string, ILogInfo>())
+            .set(propertyName, info);
     }
 }
 
