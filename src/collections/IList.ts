5ba788f307a6deacd6f5f58f6fe8cd253cbb745b
FlipSs <flipss1599@gmail.com>
2020-02-02 13:12:02 +0300

    fixes

--- src/collections/IList.ts
+++ src/collections/IList.ts
@@ -1,8 +1,6 @@
 import {IReadOnlyList} from "./internal";
 
 export interface IList<T> extends IReadOnlyList<T> {
-    get(index: number): T;
-
     add(value: T): void;
 
     addRange(values: Iterable<T>): void
--- src/collections/IReadOnlyList.ts
+++ src/collections/IReadOnlyList.ts
@@ -1,4 +1,5 @@
 import {IReadOnlyCollection} from "./IReadOnlyCollection";
 
 export interface IReadOnlyList<T> extends IReadOnlyCollection<T> {
+    get(index: number): T;
 }
