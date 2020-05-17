f69127729aa47e0b02e4640f341bf0939e3b5014
FlipSs <flipss1599@gmail.com>
2020-05-17 09:54:17 +0300

    fix tests

--- tests/collections/comparers/IgnoreCaseStringComparer.spec.ts
+++ tests/collections/comparers/IgnoreCaseStringComparer.spec.ts
@@ -5,12 +5,9 @@ describe('IgnoreCaseStringComparer', () => {
 
     beforeEach(() => {
         comparer = new IgnoreCaseStringComparer();
-    })
+    });
 
     it('Should ignore string case', () => {
-        expect(comparer.equals(null, null)).toBeTrue();
-        expect(comparer.equals(undefined, null)).toBeFalse();
-        expect(comparer.equals('', null)).toBeFalse();
         expect(comparer.equals('', '')).toBeTrue();
         expect(comparer.equals('test', 'test')).toBeTrue();
         expect(comparer.equals('test', 'tEst')).toBeTrue();
